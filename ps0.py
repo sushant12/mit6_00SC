996a679007577dae9fd91e26952c4aaff907cdde
sushant12 <sushatnbajracharya@gmail.com>
2015-06-06 09:47:52 +0545

    added pset0

---
+++ ps0.py
@@ -0,0 +1,7 @@
+#Problem Set 0
+#Name: Sushant Bajracharya(sussyoung9@gmail.com)
+#Time Spent: 30 sec
+dob = raw_input("Enter your date of birth: ")
+lname = raw_input("Enter your last name: ")
+
+print lname,dob
